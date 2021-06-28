dff7cba234670da7a01ad2a8279d921bd09d773e
unknown <mariamacedo2xx@gmail.com>
2021-06-27 21:46:50 -0300

    Iniciando proojeto e configurando redux e tailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
 
-function App() {
+const App = (props) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p class="m-8 ">hello world</p>
     </div>
   );
-}
+};
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    user: dispatch.user,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
