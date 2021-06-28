ae3e350c79458a9c1e7df962bf46e8167d585e79
unknown <mariamacedo2xx@gmail.com>
2021-06-28 13:38:48 -0300

    terminando os menus do usuario logado e deslogado

--- src/components/partials/Header.js
+++ src/components/partials/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { isLogged } from "../../helpers/AuthHandler";
+
 const Header = () => {
+  let logged = isLogged();
+
   return (
     <div class="h-16 bg-white border-2 border-solid border-gray-300">
       <div class="max-w-screen-lg m-auto flex">
@@ -12,6 +16,48 @@ const Header = () => {
             <span class="text-4xl font-bold color text-blue-600">X</span>
           </Link>
         </div>
+        <nav class="py-2">
+          <ul class="flex items-center h-10">
+            {logged && (
+              <>
+                <li class="mx-5 hover:text-gray-600">
+                  <Link to="/my-account">Minha Conta</Link>
+                </li>
+                <li class="mx-5 hover:text-gray-600">
+                  <Link to="/logout">Sair</Link>
+                </li>
+
+                <li class="mx-5">
+                  <Link
+                    to="/post-un-ad"
+                    class="px-2 py-2 rounded bg-yellow-600 text-white transition duration-200 hover:bg-yellow-700"
+                  >
+                    Poste um anúncio
+                  </Link>
+                </li>
+              </>
+            )}
+
+            {!logged && (
+              <>
+                <li class="mx-5 hover:text-gray-600">
+                  <Link to="/signin">Login</Link>
+                </li>
+                <li class="mx-5 hover:text-gray-600">
+                  <Link to="/singup">Cadastrar</Link>
+                </li>
+                <li class="mx-5">
+                  <Link
+                    to="/signin"
+                    class="px-2 py-2 rounded bg-yellow-600 text-white transition duration-200 hover:bg-yellow-700"
+                  >
+                    Poste um anúncio
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </nav>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const store = createStore(Reducers);
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App class="font-Open" />
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
